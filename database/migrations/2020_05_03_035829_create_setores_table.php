5bc68a2d544316139401ba5b690b48eccb0c50c6
lucas rodrigues <www.lucasrodrigues98@hotmail.com>
2020-05-05 16:42:43 -0300

    erros

--- database/migrations/2020_05_03_035829_create_setores_table.php
+++ database/migrations/2020_05_03_035829_create_setores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSetorsTable extends Migration
+class CreateSetoresTable extends Migration
 {
     /**
      * Run the migrations.
