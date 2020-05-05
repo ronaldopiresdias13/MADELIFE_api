72461004ef0a0204c3ed193497453c5c844378e6
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-05-05 20:20:31 -0300

    Controllers

---
+++ app/Http/Controllers/Api/CidadesController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Cidade;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CidadesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Cidade::all()->sortBy('nome');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $cidade = new Cidade;
+        $cidade->nome = $request->nome;
+        $cidade->uf   = $request->uf;
+        $cidade->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cidade  $cidade
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cidade $cidade)
+    {
+        return $cidade;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cidade  $cidade
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cidade $cidade)
+    {
+        $cidade->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cidade  $cidade
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cidade $cidade)
+    {
+        $cidade->delete();
+    }
+}
