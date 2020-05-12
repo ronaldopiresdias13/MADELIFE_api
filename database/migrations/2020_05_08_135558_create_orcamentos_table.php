c350b77af8c2e6dd2049676a213315767f00490e
Romario Pires <romario.pires@grupolife.med.br>
2020-05-12 10:05:28 -0300

    Nullable in Orcamento e Produto

--- database/migrations/2020_05_08_135558_create_orcamentos_table.php
+++ database/migrations/2020_05_08_135558_create_orcamentos_table.php
@@ -15,17 +15,17 @@ class CreateOrcamentosTable extends Migration
     {
         Schema::create('orcamentos', function (Blueprint $table) {
             $table->id();
-            $table->string('numero');
-            $table->string('tipo');
+            $table->string('numero')->nullable();
+            $table->string('tipo')->nullable();
             $table->foreignId('cliente_id')->constrained()->onDelete('cascade');
             $table->foreignId('empresa_id')->constrained()->onDelete('cascade');
-            $table->string('data');
-            $table->integer('quantidade');
-            $table->string('unidade');
+            $table->string('data')->nullable();
+            $table->integer('quantidade')->nullable();
+            $table->string('unidade')->nullable();
             $table->unsignedBigInteger('cidade_id')->nullable();
             $table->foreign('cidade_id')->references('id')->on('cidades')->onDelete('cascade');
             $table->string('processo')->nullable();
-            $table->string('situacao');
+            $table->string('situacao')->nullable();
             $table->string('descricao')->nullable();
             $table->string('observacao')->nullable();
             $table->boolean('status')->default(true);
--- database/migrations/2020_05_08_203259_create_produtos_table.php
+++ database/migrations/2020_05_08_203259_create_produtos_table.php
@@ -16,10 +16,12 @@ class CreateProdutosTable extends Migration
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('empresa_id')->constrained()->onDelete('cascade');
-            $table->foreignId('tipoproduto_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('tipoproduto_id')->nullable();
+            $table->foreign('tipoproduto_id')->references('id')->on('tipoprodutos')->onDelete('cascade');
             $table->string('codigo')->nullable();
             $table->string('descricao');
-            $table->foreignId('unidademedida_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('unidademedida_id')->nullable();
+            $table->foreign('unidademedida_id')->references('id')->on('unidademedidas')->onDelete('cascade');
             $table->string('codigobarra')->nullable();
             $table->string('validade')->nullable();
             $table->string('grupo')->nullable();
@@ -32,9 +34,10 @@ class CreateProdutosTable extends Migration
             $table->integer('quantidadeestoque')->nullable();
             $table->string('armazem')->nullable();
             $table->string('localizacaofisica')->nullable();
-            $table->unsignedBigInteger('fornecedor_id');
+            $table->unsignedBigInteger('fornecedor_id')->nullable();
             $table->foreign('fornecedor_id')->references('id')->on('fornecedores')->onDelete('cascade');
-            $table->foreignId('marca_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('marca_id')->nullable();
+            $table->foreign('marca_id')->references('id')->on('marcas')->onDelete('cascade');
             $table->string('datacompra')->nullable();
             $table->timestamps();
         });
