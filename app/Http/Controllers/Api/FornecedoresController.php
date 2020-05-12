23ed78b8ff2f92c7df620a3040133dc6b3062bfa
Romario Pires <romario.pires@grupolife.med.br>
2020-05-12 14:32:48 -0300

    Controller e update Model Fornecedor

---
+++ app/Http/Controllers/Api/FornecedoresController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Fornecedor;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FornecedoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Fornecedor  $fornecedor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Fornecedor $fornecedor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Fornecedor  $fornecedor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Fornecedor $fornecedor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Fornecedor  $fornecedor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Fornecedor $fornecedor)
+    {
+        //
+    }
+}
