8321578b16dff0a409554c32ffd6c4c332019d58
Romario Pires <romario.pires@grupolife.med.br>
2020-05-12 16:05:55 -0300

    Migracao de Fornecedores e Unidades de Medidas

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,8 @@ Route::group([
     Route::post('orcamentos/migracao'   , 'Api\OrcamentosController@migracao'   );
     Route::post('cuidados/migracao'     , 'Api\CuidadosController@migracao'     );
     Route::post('grupocuidados/migracao', 'Api\GrupocuidadosController@migracao');
+    Route::post('fornecedores/migracao', 'Api\FornecedoresController@migracao');
+    Route::post('unidades-medidas/migracao', 'Api\UnidadesMedidasController@migracao');
 
 
 
