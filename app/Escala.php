5fed2156a0e1ab831332b14a4af7d6d65ed32edf
Romario Pires <romario.pires@grupolife.med.br>
2020-06-03 16:47:46 -0300

    Escala - Cuidados

--- app/Escala.php
+++ app/Escala.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Escala extends Model
 {
     protected $guarded = [];
+
+    public function cuidados()
+    {
+        return $this->belongsToMany('App\Cuidado', 'cuidado_escalas')
+        ->withPivot(
+            'id',
+            'data'      ,
+            'hora'      ,
+            'status'    
+        );
+    } 
 }
