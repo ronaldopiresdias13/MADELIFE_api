ac1cc3c34c52888a69fded7906e6a93fd0ca63c3
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-06-09 20:06:39 -0300

    controllers

---
+++ app/Http/Controllers/Api/ContasController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Conta;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ContasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Conta $conta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Conta $conta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Conta  $conta
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Conta $conta)
+    {
+        //
+    }
+}
