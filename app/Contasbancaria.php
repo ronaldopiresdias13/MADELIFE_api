1d8c967ff759672783b1d54e3202359b716811b3
Romario Pires <romario.pires@grupolife.med.br>
2020-06-09 19:35:22 -0300

    Conta Bancaria -> Banco

--- app/Contasbancaria.php
+++ app/Contasbancaria.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Contasbancaria extends Model
 {
     protected $guarded = [];
+
+    public function banco()
+    {
+        return $this->belongsTo('App\Banco');
+    }
 }
