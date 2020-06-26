6afd33b793e220e45811cec9d1addfdc5015c353
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-06-26 18:53:47 -0300

    patrimonio controller

---
+++ app/Http/Controllers/Api/PatrimoniosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Patrimonio;
+use Illuminate\Http\Request;
+
+class PatrimoniosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Patrimonio  $patrimonio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Patrimonio $patrimonio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Patrimonio  $patrimonio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Patrimonio $patrimonio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Patrimonio  $patrimonio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Patrimonio $patrimonio)
+    {
+        //
+    }
+}
