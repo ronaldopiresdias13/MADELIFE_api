5091b98efd7976bf6c0bc6e06dd224ca468c4374
Lucas Oliveira <lucas.oliveira@grupolife.med.br>
2020-06-27 16:06:30 -0300

    update clientes

--- app/Http/Controllers/Api/ClientesController.php
+++ app/Http/Controllers/Api/ClientesController.php
@@ -312,49 +312,59 @@ class ClientesController extends Controller
             }
             if ($request['pessoa']['telefones']) {
                 foreach ($request['pessoa']['telefones'] as $key => $telefone) {
-                    $pessoa_telefone = PessoaTelefone::firstOrCreate([
-                        'pessoa_id'   => $pessoa->id,
-                        'telefone_id' => Telefone::firstOrCreate(
-                            [
-                                'telefone'  => $telefone['telefone'],
-                            ]
-                        )->id,
-                        'tipo'      => $telefone['pivot']['tipo'],
-                        'descricao' => $telefone['pivot']['descricao'],
-                    ]);
+                    $pessoa_telefone = PessoaTelefone::firstOrCreate(
+                        [
+                            'pessoa_id'   => $pessoa->id,
+                            'telefone_id' => Telefone::firstOrCreate(
+                                [
+                                    'telefone'  => $telefone['telefone'],
+                                ]
+                            )->id,
+                        ],
+                        [
+                            'tipo'      => $telefone['pivot']['tipo'],
+                            'descricao' => $telefone['pivot']['descricao'],
+                        ]
+                    );
                 }
             }
             if ($request['pessoa']['enderecos']) {
                 foreach ($request['pessoa']['enderecos'] as $key => $endereco) {
-                    $pessoa_endereco = PessoaEndereco::firstOrCreate([
-                        'pessoa_id'   => $pessoa->id,
-                        'endereco_id' => Endereco::firstOrCreate(
-                            [
-                                'cep'         => $endereco['cep'],
-                                'cidade_id'   => $endereco['cidade_id'],
-                                'rua'         => $endereco['rua'],
-                                'bairro'      => $endereco['bairro'],
-                                'numero'      => $endereco['numero'],
-                                'complemento' => $endereco['complemento'],
-                                'tipo'        => $endereco['tipo'],
-                                'descricao'   => $endereco['descricao'],
-                            ]
-                        )->id,
-                    ]);
+                    $pessoa_endereco = PessoaEndereco::updateOrCreate(
+                        [
+                            'pessoa_id'   => $pessoa->id,
+                            'endereco_id' => Endereco::firstOrCreate(
+                                [
+                                    'cep'         => $endereco['cep'],
+                                    'cidade_id'   => $endereco['cidade_id'],
+                                    'rua'         => $endereco['rua'],
+                                    'bairro'      => $endereco['bairro'],
+                                    'numero'      => $endereco['numero'],
+                                    'complemento' => $endereco['complemento'],
+                                    'tipo'        => $endereco['tipo'],
+                                    'descricao'   => $endereco['descricao'],
+                                ]
+                            )->id,
+                        ]
+                    );
                 }
             }
             if ($request['pessoa']['emails']) {
                 foreach ($request['pessoa']['emails'] as $key => $email) {
-                    $pessoa_email = PessoaEmail::firstOrCreate([
-                        'pessoa_id' => $pessoa->id,
-                        'email_id'  => Email::firstOrCreate(
-                            [
-                                'email' => $email['email'],
-                            ]
-                        )->id,
-                        'tipo'      => $email['pivot']['tipo'],
-                        'descricao' => $email['pivot']['descricao'],
-                    ]);
+                    $pessoa_email = PessoaEmail::updateOrCreate(
+                        [
+                            'pessoa_id' => $pessoa->id,
+                            'email_id'  => Email::firstOrCreate(
+                                [
+                                    'email' => $email['email'],
+                                ]
+                            )->id,
+                        ],
+                        [
+                            'tipo'      => $email['pivot']['tipo'],
+                            'descricao' => $email['pivot']['descricao'],
+                        ]
+                    );
                 }
             }
             if ($request['pessoa']['user']) {
