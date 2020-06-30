994e06f7bb358ac46233fa83ec339971ac503fc3
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-06-30 14:39:32 -0300

    push Medicao table

---
+++ app/ServicoMedicao.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ServicoMedicao extends Model
+{
+    protected $table = 'servico_medicoes';
+    protected $guarded = [];
+}
---
+++ database/migrations/2020_06_30_172138_create_medicoes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicoes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('empresa_id');
+            $table->foreign('empresa_id')->references('id')->on('empresas')->onDelete('cascade');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->string('data1');
+            $table->string('data2');
+            $table->string('valor');
+            $table->string('situacao');
+            $table->string('observacao');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicoes');
+    }
+}
