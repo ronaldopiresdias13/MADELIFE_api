87ff0bf7b000f17551f7d9b3ce31dc4c83cda997
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2020-07-01 20:17:41 -0300

    medicso

--- app/Medicao.php
+++ app/Medicao.php
@@ -8,4 +8,9 @@ class Medicao extends Model
 {
     protected $table = 'medicoes';
     protected $guarded = [];
+
+    public function medicao_servicos()
+    {
+        return $this->hasMany('App\ServicoMedicao');
+    }
 }
