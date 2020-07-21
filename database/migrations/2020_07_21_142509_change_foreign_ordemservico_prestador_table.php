f2cefdfe2d397514104ae0cb3c577388def73347
Lucas Oliveira <lucas.oliveira@grupolife.med.br>
2020-07-21 11:48:22 -0300

    ok

---
+++ database/migrations/2020_07_21_142509_change_foreign_ordemservico_prestador_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeForeignOrdemservicoPrestadorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ordemservico_prestador', function (Blueprint $table) {
+            $table->dropForeign('atribuicoes_prestador_id_foreign');
+            $table->foreign('prestador_id')->references('id')->on('prestadores')->onDelete('cascade');
+            $table->dropForeign('atribuicoes_ordemservico_id_foreign');
+            $table->foreign('ordemservico_id')->references('id')->on('ordemservicos')->onDelete('cascade');
+
+            $table->renameIndex('atribuicoes_prestador_id_foreign', 'ordemservico_prestador_prestador_id_foreign');
+            $table->renameIndex('atribuicoes_ordemservico_id_foreign', 'ordemservico_prestador_ordemservico_id_foreign');
+            $table->renameIndex('atribuicoes_ativo_index', 'ordemservico_prestador_ativo_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
