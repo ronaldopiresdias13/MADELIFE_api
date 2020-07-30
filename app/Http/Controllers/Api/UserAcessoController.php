c4b303920bc7a81a4a857f5569b86e7a9e0cbad8
Romario Pires <romario.pires@grupolife.med.br>
2020-07-30 16:22:06 -0300

    User

--- app/Http/Controllers/Api/UserAcessoController.php
+++ app/Http/Controllers/Api/UserAcessoController.php
@@ -60,6 +60,6 @@ class UserAcessoController extends Controller
      */
     public function destroy(UserAcesso $userAcesso)
     {
-        //
+        $userAcesso->delete();
     }
 }
--- app/User.php
+++ app/User.php
@@ -62,6 +62,6 @@ class User extends Authenticatable
 
     public function acessos()
     {
-        return $this->belongsToMany('App\Acesso', 'user_acesso')->wherePivot('ativo', true);
+        return $this->belongsToMany('App\Acesso', 'user_acesso')->withPivot('id')->wherePivot('ativo', true);
     }
 }
