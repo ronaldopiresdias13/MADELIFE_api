f1e232c2fa3cfb0f3a10cc8ddb705365ebe702d6
lucasprogrammerads <lucas.oliveira@grupolife.med.br>
2020-08-25 22:31:14 -0300

    eager loading

--- app/Http/Controllers/Api/EscalasController.php
+++ app/Http/Controllers/Api/EscalasController.php
@@ -20,7 +20,29 @@ class EscalasController extends Controller
      */
     public function index(Request $request)
     {
-        $itens = Escala::where('ativo', true);
+        $with = [];
+
+        if ($request['adicionais']) {
+            foreach ($request['adicionais'] as $key => $adicional) {
+                if (is_string($adicional)) {
+                    array_push($with, $adicional);
+                } else {
+                    $filho = '';
+                    foreach ($adicional as $key => $a) {
+                        if ($key == 0) {
+                            $filho = $a;
+                        } else {
+                            $filho = $filho . '.' . $a;
+                        }
+                    }
+                    array_push($with, $filho);
+                }
+            }
+            $itens = Escala::with($with)->where('ativo', true);
+        } else {
+            $itens = Escala::where('ativo', true);
+        }
+
 
         if ($request->commands) {
             $request = json_decode($request->commands, true);
--- app/Http/Controllers/Teste.php
+++ app/Http/Controllers/Teste.php
@@ -2,14 +2,107 @@
 
 namespace App\Http\Controllers;
 
-use App\Cnab;
 use App\Escala;
 use Illuminate\Http\Request;
 
 class Teste extends Controller
 {
-    public function teste()
+    public function teste(Request $request)
     {
+        $with = [];
+
+        if ($request['adicionais']) {
+            foreach ($request['adicionais'] as $key => $adicional) {
+                if (is_string($adicional)) {
+                    array_push($with, $adicional);
+                } else {
+                    $filho = '';
+                    foreach ($adicional as $key => $a) {
+                        if ($key == 0) {
+                            $filho = $a;
+                        } else {
+                            $filho = $filho . '.' . $a;
+                        }
+                    }
+                    array_push($with, $filho);
+                }
+            }
+            $itens = Escala::with($with)->where('ativo', true);
+        } else {
+            $itens = Escala::where('ativo', true);
+        }
+
+        // $itens->where('pontos.ativo', 'like', true);
+
+        if ($request->commands) {
+            $request = json_decode($request->commands, true);
+        }
+
+        if ($request['where']) {
+            foreach ($request['where'] as $key => $where) {
+                $itens->where(
+                    ($where['coluna']) ? $where['coluna'] : 'id',
+                    ($where['expressao']) ? $where['expressao'] : 'like',
+                    ($where['valor']) ? $where['valor'] : '%'
+                );
+            }
+        }
+
+        if ($request['order']) {
+            foreach ($request['order'] as $key => $order) {
+                $itens->orderBy(
+                    ($order['coluna']) ? $order['coluna'] : 'id',
+                    ($order['tipo']) ? $order['tipo'] : 'asc'
+                );
+            }
+        }
+
+        // if ($request['take']) {
+        //     $itens->take($request['take']);
+        // }
+
+        // if ($request['limit']) {
+        //     $itens->limit($request['limit']);
+        // }
+
+        // $itens->limit(5);
+
+        $itens = $itens->get();
+
+        if ($request['adicionais']) {
+            foreach ($itens as $key => $iten) {
+                foreach ($request['adicionais'] as $key => $adicional) {
+                    if (is_string($adicional)) {
+                        $iten[$adicional];
+                    } else {
+                        $iten2 = $iten;
+                        foreach ($adicional as $key => $a) {
+                            if ($key == 0) {
+                                if ($iten[0] == null) {
+                                    $iten2 = $iten[$a];
+                                } else {
+                                    foreach ($iten as $key => $i) {
+                                        $i[$a];
+                                    }
+                                }
+                            } else {
+                                if ($iten2[0] == null) {
+                                    $iten2 = $iten2[$a];
+                                } else {
+                                    foreach ($iten2 as $key => $i) {
+                                        $i[$a];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // return $itens->paginate(5);
+        return $itens;
+
         // "where": [
         //     {
         //       "coluna": "empresa_id",
@@ -38,9 +131,9 @@ class Teste extends Controller
 
 
         $escalas = Escala::where('ativo', true)
-        ->where('empresa_id', 1)->where('ordemservico.id', 129)
-        ->orderBy('dataentrada')
-        ->get();
+            ->where('empresa_id', 1)->where('ordemservico.id', 129)
+            ->orderBy('dataentrada')
+            ->get();
 
         foreach ($escalas as $key => $escala) {
             $escala->servico;
