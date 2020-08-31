6b3129a16ff7f86ae54b5d160065bbb5ba7460e8
Romario Pires <romario.pires@grupolife.med.br>
2020-08-31 10:29:32 -0300

    TipoPessoa

--- routes/api.php
+++ routes/api.php
@@ -507,6 +507,12 @@ Route::get('telefones/{telefone}', 'Api\TelefonesController@show');
 Route::put('telefones/{telefone}', 'Api\TelefonesController@update');
 Route::delete('telefones/{telefone}', 'Api\TelefonesController@destroy');
 
+Route::get('tipopessoas', 'Api\TipopessoasController@index');
+Route::post('tipopessoas', 'Api\TipopessoasController@store');
+Route::get('tipopessoas/{tipopessoa}', 'Api\TipopessoasController@show');
+Route::put('tipopessoas/{tipopessoa}', 'Api\TipopessoasController@update');
+Route::delete('tipopessoas/{tipopessoa}', 'Api\TipopessoasController@destroy');
+
 Route::get('tipoprodutos', 'Api\TipoprodutosController@index');
 Route::post('tipoprodutos', 'Api\TipoprodutosController@store');
 Route::get('tipoprodutos/{tipoproduto}', 'Api\TipoprodutosController@show');
