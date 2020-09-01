193b4c4d51e654e7b56eba72e7b9bf8aa0061413
lucasprogrammerads <lucas.oliveira@grupolife.med.br>
2020-09-01 15:47:11 -0300

    novas funcionalidades

--- app/Mail/LogApp.php
+++ app/Mail/LogApp.php
@@ -2,29 +2,28 @@
 
 namespace App\Mail;
 
-use App\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
-class ResetPassword extends Mailable
+class LogApp extends Mailable
 {
     use Queueable;
     use SerializesModels;
 
-    private $user;
-    private $senha;
+    private $log;
+    private $email;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(User $user, $senha)
+    public function __construct($log)
     {
-        $this->user  = $user;
-        $this->senha = $senha;
+        $this->log = $log;
+        $this->email = 'suporte@madelife.med.br';
     }
 
     /**
@@ -35,9 +34,9 @@ class ResetPassword extends Mailable
     public function build()
     {
         $this->subject('Recuperação de senha MadeLife');
-        $this->to($this->user->email, $this->user->pessoa->nome);
-        return $this->view('mail.ResetPassword', [
-            'user' => $this->user, 'senha' => $this->senha
+        $this->to($this->email, $this->log);
+        return $this->view('mail.LogApp', [
+            'log' => $this->log
         ]);
     }
 }
