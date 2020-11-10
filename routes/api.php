7db9e392ce70dfacdc58a532c6c242d55671e396
Romario Pires <romario.pires@grupolife.med.br>
2020-11-10 13:59:59 -0300

    Vendas

--- routes/api.php
+++ routes/api.php
@@ -713,3 +713,4 @@ Route::post('vendas', 'Api\VendasController@store');
 Route::get('vendas/{venda}', 'Api\VendasController@show');
 Route::put('vendas/{venda}', 'Api\VendasController@update');
 Route::delete('vendas/{venda}', 'Api\VendasController@destroy');
+Route::post('vendas/cadastrarCliente', 'Api\VendasController@cadastrarCliente');
