e4c1c8f3519e088aa1c7119c9957b3467e044bac
Romario Pires <romario.pires@grupolife.med.br>
2020-11-10 16:22:52 -0300

    Entrada

--- app/Entrada.php
+++ app/Entrada.php
@@ -9,7 +9,8 @@ class Entrada extends Model
 {
     use Uuid;
 
-    // protected $keyType = 'string';
+    protected $keyType = 'string';
+    public $incrementing = false;
     // protected $primaryKey = 'uuid';
     protected $guarded = [];
 
