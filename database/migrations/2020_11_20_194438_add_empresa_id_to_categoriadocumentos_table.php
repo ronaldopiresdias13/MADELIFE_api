31e4737d779c03a754dc2a44737abf82c4099ce8
lucas.oliveira <lucas.oliveira@grupolife.med.br>
2020-11-23 10:55:28 -0300

    correção

--- database/migrations/2020_11_20_194438_add_empresa_id_to_categoriadocumentos_table.php
+++ database/migrations/2020_11_20_194438_add_empresa_id_to_categoriadocumentos_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Categoriadocumento;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,8 +17,19 @@ class AddEmpresaIdToCategoriadocumentosTable extends Migration
         Schema::table('categoriadocumentos', function (Blueprint $table) {
             $table->unsignedBigInteger('empresa_id')->nullable()->after('id');
             $table->foreign('empresa_id')->references('id')->on('empresas');
-            $table->dropColumn('ativo');
             $table->softDeletes();
+        });
+
+        $categoriadocumentos = Categoriadocumento::all();
+
+        foreach ($categoriadocumentos as $key => $categoriadocumento) {
+            if (!$categoriadocumento->ativo) {
+                $categoriadocumento->delete();
+            }
+        }
+
+        Schema::table('categoriadocumentos', function (Blueprint $table) {
+            $table->dropColumn('ativo');
         });
     }
 
@@ -32,6 +44,18 @@ class AddEmpresaIdToCategoriadocumentosTable extends Migration
             $table->dropForeign(['empresa_id']);
             $table->dropColumn('empresa_id');
             $table->boolean('ativo')->default(true)->after('categoria');
+        });
+
+        $categoriadocumentos = Categoriadocumento::withTrashed()->get();
+
+        foreach ($categoriadocumentos as $key => $categoriadocumento) {
+            if ($categoriadocumento->deleted_at != null) {
+                $categoriadocumento->ativo = false;
+                $categoriadocumento->save();
+            }
+        }
+
+        Schema::table('categoriadocumentos', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
     }
