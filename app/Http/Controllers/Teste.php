2db9e977da3bd4a5a42fda658e15be9e98bb9fce
lucas.oliveira <lucas.oliveira@grupolife.med.br>
2020-12-17 18:53:49 -0300

    correções

--- app/Http/Controllers/Teste.php
+++ app/Http/Controllers/Teste.php
@@ -4,13 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Banco;
 use App\Ordemservico;
+use App\OrdemservicoAcesso;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class Teste extends Controller
 {
-    public function teste(Request $request)
+    public function teste(Ordemservico $ordemservico)
     {
-        return Banco::orderBy('codigo')->get();
+        $acessos = $ordemservico->empresa->acessos;
+
+        foreach ($acessos as $key => $acesso) {
+            DB::transaction(function () use ($ordemservico, $acesso) {
+                OrdemservicoAcesso::create(
+                    [
+                        'empresa_id'      => $ordemservico->empresa_id,
+                        'ordemservico_id' => $ordemservico->id,
+                        'acesso_id'       => $acesso->id
+                    ]
+                );
+            });
+        }
     }
 }
