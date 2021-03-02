4c75aea20dff04982a0170dc18e5784bb7bec68d
Matheus Carreira Andrade <matheuscarreira1996@gmail.com>
2021-03-02 16:58:09 -0300

    cnabs

---
+++ app/Http/Controllers/Api/Web/Financeiro/CnabsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Web\Financeiro;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CnabsController extends Controller
+{
+    //
+}
