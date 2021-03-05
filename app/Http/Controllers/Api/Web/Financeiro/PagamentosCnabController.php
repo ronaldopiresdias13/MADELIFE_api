00cc1100693f076faae2cd2bf94148c8a963d538
Romario Pires <romario.pires@grupolife.med.br>
2021-03-05 17:47:06 -0300

    Pag Cnab

---
+++ app/Http/Controllers/Api/Web/Financeiro/PagamentosCnabController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Api\Web\Financeiro;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pagamentoexterno;
+use App\Models\Pagamentointerno;
+use Illuminate\Http\Request;
+
+class PagamentosCnabController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function listPagamentosByEmpresaId(Request $request)
+    {
+        $result = [];
+        $pagamentos = [];
+        $user = $request->user();
+        $empresa_id = $user->pessoa->profissional->empresa_id;
+        if ($request->tipo == "Prestador") {
+            $pagamentos = Pagamentoexterno::with(['pessoa.dadosbancario.banco'])
+                ->where('empresa_id', $empresa_id)
+                ->where('status', false)
+                ->where('situacao', "!=", "Criado")
+                ->whereBetween('dataentrada', [$request->data_ini, $request->data_fim])
+                ->get();
+        }
+        if ($request->tipo == "Profissional") {
+            $pagamentos = Pagamentointerno::with(['pessoa.dadosbancario.banco'])
+                ->where('empresa_id', $empresa_id)
+                ->where('status', false)
+                ->where('situacao', "!=", "Criado")
+                ->whereBetween('dataentrada', [$request->data_ini, $request->data_fim])
+                ->get();
+        } else {
+            return $result;
+        }
+
+        // ->groupBy("pessoa_id");
+        // ->keyBy("pessoa_id")
+
+        foreach ($pagamentos as $key => $pagamento) {
+            $tem = false;
+            foreach ($result as $key => $r) {
+                if ($r['profissional'] == $pagamento->pessoa_id) {
+                    array_push($result[$key]['pagamentos'], $pagamento);
+                    $tem = true;
+                    break;
+                }
+            }
+            if (!$tem) {
+                $array = [
+                    'profissional' => $pagamento->pessoa_id,
+                    'nome'         => $pagamento->pessoa->nome,
+                    'pagamentos'   => [$pagamento]
+                ];
+                array_push($result, $array);
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
