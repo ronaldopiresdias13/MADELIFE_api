667fbf58a1e4afb734efcbc5e62851ae7b182c90
Matheus Carreira Andrade <matheuscarreira1996@gmail.com>
2021-03-29 17:09:58 -0300

    wss

--- app/Console/Commands/ChamadoWebSocketServer.php
+++ app/Console/Commands/ChamadoWebSocketServer.php
@@ -38,6 +38,23 @@ class ChamadoWebSocketServer extends Command
      */
     public function handle()
     {
+         // $loop = \React\EventLoop\Factory::create();
+
+        // $secure_websockets = new \React\Socket\Server('0.0.0.0:10990', $loop);
+        // $secure_websockets = new \React\Socket\SecureServer($secure_websockets, $loop, [
+        //     'local_cert' => '/etc/letsencrypt/live/madelife.enterscience.com.br/fullchain.pem',
+        //     'local_pk' => '/etc/letsencrypt/live/madelife.enterscience.com.br/privkey.pem',
+        //     'verify_peer' => false
+        // ]);
+
+        // $app = new \Ratchet\Http\HttpServer(
+        //     new \Ratchet\WebSocket\WsServer(
+        //         new ChamadoWebSocketController()
+        //     )
+        // );
+        // $server = new \Ratchet\Server\IoServer($app, $secure_websockets, $loop);
+        // $server->run();
+
         $loop_ = \React\EventLoop\Factory::create();
         $socket_ = new \React\Socket\Server('0.0.0.0:10990', $loop_);
         $app_ = new \Ratchet\Http\HttpServer(
