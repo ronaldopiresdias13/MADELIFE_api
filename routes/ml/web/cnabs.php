b50481bf86a894577e992d012089d2ece872fd5f
lucas.oliveira <lucas.oliveira@grupolife.med.br>
2021-04-14 17:11:41 -0300

    correção rotas

--- routes/ml/web/cnabs.php
+++ routes/ml/web/cnabs.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('financeiro')->group(function () {
-    Route::get('getCategorias', 'Api\Web\Financeiro\CnabsController@getCategorias');
+Route::prefix('web')->group(function () {
+    Route::prefix('financeiro')->group(function () {
+        Route::get('getCategorias', 'Api\Web\Financeiro\CnabsController@getCategorias');
 
-    Route::post('gerarCnab', 'Api\Web\Financeiro\CnabsController@gerarCnab');
-    Route::post('mudarSituacao', 'Api\Web\Financeiro\CnabsController@mudarSituacao');
+        Route::post('gerarCnab', 'Api\Web\Financeiro\CnabsController@gerarCnab');
+        Route::post('mudarSituacao', 'Api\Web\Financeiro\CnabsController@mudarSituacao');
 
-    Route::get('downloadCnab/{id}', 'Api\Web\Financeiro\CnabsController@downloadCnab');
-    Route::get('getCnabs', 'Api\Web\Financeiro\CnabsController@getCnabs');
+        Route::get('downloadCnab/{id}', 'Api\Web\Financeiro\CnabsController@downloadCnab');
+        Route::get('getCnabs', 'Api\Web\Financeiro\CnabsController@getCnabs');
+    });
 });
--- routes/ml/web/pagamentopessoas.php
+++ routes/ml/web/pagamentopessoas.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('financeiro')->group(function () {
-    Route::get('listPagamentosByEmpresaId', 'Api\Web\Financeiro\PagamentopessoasController@listPagamentosByEmpresaId');
+Route::prefix('web')->group(function () {
+    Route::prefix('financeiro')->group(function () {
+        Route::get('listPagamentosByEmpresaId', 'Api\Web\Financeiro\PagamentopessoasController@listPagamentosByEmpresaId');
+    });
 });
