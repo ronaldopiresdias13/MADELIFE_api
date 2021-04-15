ad953ea1ca92127adc1e9143e6fb03913775feac
Romario Pires <romario.pires@grupolife.med.br>
2021-04-15 16:08:55 -0300

    Correção rota getCnabs

--- routes/ml/web/cnabs.php
+++ routes/ml/web/cnabs.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('web')->group(function () {
-    Route::prefix('financeiro')->group(function () {
-        Route::get('getCategorias', 'Api\Web\Financeiro\CnabsController@getCategorias');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::prefix('web')->group(function () {
+        Route::prefix('financeiro')->group(function () {
+            Route::get('getCategorias', 'Api\Web\Financeiro\CnabsController@getCategorias');
 
-        Route::post('gerarCnab', 'Api\Web\Financeiro\CnabsController@gerarCnab');
-        Route::post('mudarSituacao', 'Api\Web\Financeiro\CnabsController@mudarSituacao');
+            Route::post('gerarCnab', 'Api\Web\Financeiro\CnabsController@gerarCnab');
+            Route::post('mudarSituacao', 'Api\Web\Financeiro\CnabsController@mudarSituacao');
 
-        Route::get('downloadCnab/{id}', 'Api\Web\Financeiro\CnabsController@downloadCnab');
-        Route::get('getCnabs', 'Api\Web\Financeiro\CnabsController@getCnabs');
+            Route::get('downloadCnab/{id}', 'Api\Web\Financeiro\CnabsController@downloadCnab');
+            Route::get('getCnabs', 'Api\Web\Financeiro\CnabsController@getCnabs');
+        });
     });
 });
