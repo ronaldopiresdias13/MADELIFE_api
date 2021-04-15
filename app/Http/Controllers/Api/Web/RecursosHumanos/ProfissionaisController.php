aa8d927be2eb704a6a5d3fdc138584703dc4e328
Romario Pires <romario.pires@grupolife.med.br>
2021-04-15 16:47:35 -0300

    Correção Update profisisonal

--- app/Http/Controllers/Api/Web/RecursosHumanos/ProfissionaisController.php
+++ app/Http/Controllers/Api/Web/RecursosHumanos/ProfissionaisController.php
@@ -275,6 +275,149 @@ class ProfissionaisController extends Controller
         ], 200)
             ->header('Content-Type', 'application/json');
     }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Profissional  $profissional
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profissional $profissional)
+    {
+        DB::transaction(function () use ($request, $profissional) {
+            $profissional = Profissional::updateOrCreate(
+                [
+                    'id' => $request['id'],
+                ],
+                [
+                    'pessoafisica' => 1,
+                    'empresa_id'   => $request['empresa_id'],
+                    'pessoa_id'    => Pessoa::updateOrCreate(
+                        [
+                            // 'id' => ($request['pessoa']['id'] != '') ? $request['pessoa']['id'] : null,
+                            'id' => $request['pessoa_id'],
+                        ],
+                        [
+                            // 'empresa_id'  => $request['pessoa']['empresa_id'],
+                            'nome'        => $request['pessoa']['nome'],
+                            'nascimento'  => $request['pessoa']['nascimento'],
+                            'rgie'        => $request['pessoa']['rgie'],
+                            'observacoes' => $request['pessoa']['observacoes'],
+                            'perfil'      => $request['pessoa']['perfil'],
+                            'status'      => $request['pessoa']['status'],
+                        ]
+                    )->id,
+                    'sexo'                   => $request['sexo'],
+                    'setor_id'               => $request['setor_id'],
+                    'cargo_id'               => $request['cargo_id'],
+                    'pis'                    => $request['pis'],
+                    'numerocarteiratrabalho' => $request['numerocarteiratrabalho'],
+                    'numerocnh'              => $request['numerocnh'],
+                    'categoriacnh'           => $request['categoriacnh'],
+                    'validadecnh'            => $request['validadecnh'],
+                    'numerotituloeleitor'    => $request['numerotituloeleitor'],
+                    'zonatituloeleitor'      => $request['zonatituloeleitor'],
+                    'meiativa'               => $request['meiativa'],
+                    'dataverificacaomei'     => $request['dataverificacaomei'],
+                    'dadoscontratuais_id'    => Dadoscontratual::updateOrCreate([
+                        'tiposalario'             => $request['dadoscontratuais']['tiposalario'],
+                        'salario'                 => $request['dadoscontratuais']['salario'],
+                        'cargahoraria'            => $request['dadoscontratuais']['cargahoraria'],
+                        'insalubridade'           => $request['dadoscontratuais']['insalubridade'],
+                        'percentualinsalubridade' => $request['dadoscontratuais']['percentualinsalubridade'],
+                        'admissao'                => $request['dadoscontratuais']['admissao'],
+                        'demissao'                => $request['dadoscontratuais']['demissao'],
+                    ])->id,
+                ]
+            );
+            if ($request['formacoes']) {
+                foreach ($request['formacoes'] as $key => $formacao) {
+                    $profissional_formacao = ProfissionalFormacao::firstOrCreate([
+                        'profissional_id' => $profissional->id,
+                        'formacao_id'     => $formacao['id'],
+                    ]);
+                }
+            }
+            if ($request['beneficios']) {
+                foreach ($request['beneficios'] as $key => $beneficio) {
+                    $profissional_beneficio = ProfissionalBeneficio::firstOrCreate([
+                        'profissional_id' => $profissional->id,
+                        'beneficio_id'    => $beneficio['id']
+                    ]);
+                }
+            }
+            if ($request['convenios']) {
+                foreach ($request['convenios'] as $key => $convenio) {
+                    $profissional_convenio = ProfissionalConvenio::firstOrCreate([
+                        'profissional_id' => $profissional->id,
+                        'convenio_id'    => $convenio['id']
+                    ]);
+                }
+            }
+            if ($request['dadosBancario']) {
+                foreach ($request['dadosBancario'] as $key => $dadosbancario) {
+                    $dados_bancario = Dadosbancario::firstOrCreate([
+                        'empresa_id'  => $request['empresa_id'],
+                        'banco_id'    => $dadosbancario['banco_id'],
+                        'agencia'     => $dadosbancario['agencia'],
+                        'conta'       => $dadosbancario['conta'],
+                        'digito'      => $dadosbancario['digito'],
+                        'tipoconta'   => $dadosbancario['tipoconta'],
+                        'pessoa_id'   => $profissional->pessoa_id,
+                    ]);
+                }
+            }
+            if ($request['pessoa']['enderecos']) {
+                foreach ($request['pessoa']['enderecos'] as $key => $endereco) {
+                    $pessoa_endereco = PessoaEndereco::firstOrCreate([
+                        'pessoa_id'   => $profissional->pessoa_id,
+                        'endereco_id' => Endereco::firstOrCreate(
+                            [
+                                'cep'         => $endereco['cep'],
+                                'cidade_id'   => $endereco['cidade_id'],
+                                'rua'         => $endereco['rua'],
+                                'bairro'      => $endereco['bairro'],
+                                'numero'      => $endereco['numero'],
+                                'complemento' => $endereco['complemento'],
+                                'tipo'        => $endereco['tipo'],
+                                'descricao'   => $endereco['descricao'],
+                            ]
+                        )->id,
+                    ]);
+                }
+            }
+
+            if ($request['pessoa']['telefones']) {
+                foreach ($request['pessoa']['telefones'] as $key => $telefone) {
+                    $pessoa_telefone = PessoaTelefone::firstOrCreate([
+                        'pessoa_id'   => $profissional->pessoa_id,
+                        'telefone_id' => Telefone::firstOrCreate(
+                            [
+                                'telefone'  => $telefone['telefone'],
+                            ]
+                        )->id,
+                        'tipo'      => $telefone['pivot']['tipo'],
+                        'descricao' => $telefone['pivot']['descricao'],
+                    ]);
+                }
+            }
+
+            if ($request['pessoa']['emails']) {
+                foreach ($request['pessoa']['emails'] as $key => $email) {
+                    $pessoa_email = PessoaEmail::firstOrCreate([
+                        'pessoa_id' => $profissional->pessoa_id,
+                        'email_id'  => Email::firstOrCreate(
+                            [
+                                'email' => $email['email'],
+                            ]
+                        )->id,
+                        'tipo'      => $email['pivot']['tipo'],
+                        'descricao' => $email['pivot']['descricao'],
+                    ]);
+                }
+            }
+        });
+    }
 
     /**
      * Display the specified resource.
