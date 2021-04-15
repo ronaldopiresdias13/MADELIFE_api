bf189cd7988243464342f2260399939e868c451f
lucas.oliveira <lucas.oliveira@grupolife.med.br>
2021-04-15 17:54:20 -0300

    correção rotas

--- routes/ml/auth.php
+++ routes/ml/auth.php
@@ -18,20 +18,20 @@ Route::group([
     });
 });
 
-Route::group([
-    'prefix' => 'app/v3_0_20/auth'
-], function () {
-    Route::post('login', 'Api\App\v3_0_20\Auth\AuthController@login');
-    Route::post('register', 'Api\App\v3_0_20\Auth\AuthController@register');
-    Route::post('reset', 'Api\App\v3_0_20\Auth\AuthController@reset');
+// Route::group([
+//     'prefix' => 'app/v3_0_20/auth'
+// ], function () {
+//     Route::post('login', 'Api\App\v3_0_20\Auth\AuthController@login');
+//     Route::post('register', 'Api\App\v3_0_20\Auth\AuthController@register');
+//     Route::post('reset', 'Api\App\v3_0_20\Auth\AuthController@reset');
 
-    /* ------------- Rotas Utilizando Token -------------*/
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::post('change', 'Api\App\v3_0_20\Auth\AuthController@change');
-        Route::get('logout', 'Api\App\v3_0_20\Auth\AuthController@logout');
-        // Route::get('user', 'Auth\AuthController@user');
-    });
-});
+//     /* ------------- Rotas Utilizando Token -------------*/
+//     Route::group(['middleware' => 'auth:api'], function () {
+//         Route::post('change', 'Api\App\v3_0_20\Auth\AuthController@change');
+//         Route::get('logout', 'Api\App\v3_0_20\Auth\AuthController@logout');
+//         // Route::get('user', 'Auth\AuthController@user');
+//     });
+// });
 Route::group([
     'prefix' => 'app/v3_0_21/auth'
 ], function () {
--- routes/ml/web/pagamentopessoas.php
+++ routes/ml/web/pagamentopessoas.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('web')->group(function () {
-    Route::prefix('financeiro')->group(function () {
-        Route::get('listPagamentosByEmpresaId', 'Api\Web\Financeiro\PagamentopessoasController@listPagamentosByEmpresaId');
+Route::group(['middleware' => 'auth:api'], function () {
+
+    Route::prefix('web')->group(function () {
+        Route::prefix('financeiro')->group(function () {
+            Route::get('listPagamentosByEmpresaId', 'Api\Web\Financeiro\PagamentopessoasController@listPagamentosByEmpresaId');
+        });
     });
 });
