e66df03b3234fe42dfefb79c978b1de8e55b8162
Carlos Eduardo Pasquini <carlosed20091@gmail.com>
2021-06-01 16:09:06 -0300

    Profissionais controller

--- app/Http/Controllers/Api/Web/RecursosHumanos/ProfissionaisController.php
+++ app/Http/Controllers/Api/Web/RecursosHumanos/ProfissionaisController.php
@@ -334,7 +334,7 @@ class ProfissionaisController extends Controller
                 foreach ($request['formacoes'] as $key => $formacao) {
                     $profissional_formacao = ProfissionalFormacao::firstOrCreate([
                         'profissional_id' => $profissional->id,
-                        'formacao_id'     => $formacao['id'],
+                        'formacao_id'     => $formacao['formacao_id'],
                     ]);
                 }
             }
