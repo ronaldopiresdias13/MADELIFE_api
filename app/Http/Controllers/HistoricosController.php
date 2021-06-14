08e59774dccfdfdeebb23b577910a5a6360c2760
Romario Pires <romario.pires@grupolife.med.br>
2021-06-14 12:40:59 -0300

    Historico

--- app/Http/Controllers/HistoricosController.php
+++ app/Http/Controllers/HistoricosController.php
@@ -19,34 +19,34 @@ class HistoricosController extends Controller
         $empresa_id = $user->pessoa->profissional->empresa_id;
         return DB::select(
             "SELECT DATE_FORMAT(h.created_at, '%d/%m/%Y') AS datahistorico, DATE_FORMAT(h.created_at, '%H:%i') AS hora, p.nome, e.id AS escala_id, ps.nome AS paciente, e.dataentrada, e.datasaida, e.periodo, e.horaentrada, e.horasaida, e.status, e.ativo, prof.nome AS prestador, s.descricao,
-CASE h.tipo when 1 then 'Criado' when 2 then 'Alterado' when 3 then 'Excluído' end as operacao 
-FROM historicos AS h
-INNER JOIN escalas AS e
-ON e.id = h.historico_id
-INNER JOIN ordemservicos AS os
-ON os.id = e.ordemservico_id
-INNER JOIN orcamentos AS o
-ON o.id = os.orcamento_id
-INNER JOIN homecares AS hc
-ON hc.orcamento_id = o.id
-INNER JOIN pacientes AS pc
-ON pc.id = hc.paciente_id
-INNER JOIN pessoas AS ps
-ON ps.id = pc.pessoa_id
-INNER JOIN servicos AS s
-ON s.id = e.servico_id
-INNER JOIN users AS u
-ON u.id = h.user_id
-INNER JOIN pessoas AS p
-ON p.id = u.pessoa_id
-INNER JOIN profissionais AS pf
-ON pf.pessoa_id = p.id
-INNER JOIN prestadores AS pr
-ON pr.id = e.prestador_id
-INNER JOIN pessoas AS prof
-ON prof.id = pr.pessoa_id
-WHERE e.empresa_id = ?
-ORDER BY h.created_at desc",
+                CASE h.tipo when 1 then 'Criado' when 2 then 'Alterado' when 3 then 'Excluído' end as operacao 
+                FROM historicos AS h
+                INNER JOIN escalas AS e
+                ON e.id = h.historico_id
+                INNER JOIN ordemservicos AS os
+                ON os.id = e.ordemservico_id
+                INNER JOIN orcamentos AS o
+                ON o.id = os.orcamento_id
+                INNER JOIN homecares AS hc
+                ON hc.orcamento_id = o.id
+                INNER JOIN pacientes AS pc
+                ON pc.id = hc.paciente_id
+                INNER JOIN pessoas AS ps
+                ON ps.id = pc.pessoa_id
+                INNER JOIN servicos AS s
+                ON s.id = e.servico_id
+                INNER JOIN users AS u
+                ON u.id = h.user_id
+                INNER JOIN pessoas AS p
+                ON p.id = u.pessoa_id
+                INNER JOIN profissionais AS pf
+                ON pf.pessoa_id = p.id
+                INNER JOIN prestadores AS pr
+                ON pr.id = e.prestador_id
+                INNER JOIN pessoas AS prof
+                ON prof.id = pr.pessoa_id
+                WHERE e.empresa_id = ?
+                ORDER BY h.created_at desc",
             [
                 $empresa_id,
             ]
