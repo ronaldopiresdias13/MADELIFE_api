756798dc9be983251c778e0000f725a519431b58
Carlos Eduardo Pasquini de Paula <carlosed2009@hotmail.com>
2021-06-15 18:01:17 -0300

    EmpresaController

--- routes/ml/web/empresas.php
+++ routes/ml/web/empresas.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::prefix('web')->group(function () {
+        Route::prefix('Empresas')->group(function () {
+            Route::get('empresa', 'Web\Empresas\EmpresasController@show');
+        });
+    });
+});
+
 Route::get('empresas', 'Api\EmpresasController@index');
 Route::post('empresas', 'Api\EmpresasController@store');
 Route::get('empresas/{empresa}', 'Api\EmpresasController@show');
