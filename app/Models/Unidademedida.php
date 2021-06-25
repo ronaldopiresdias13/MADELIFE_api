b86a6451cd6b1bef7cc1e528b21e0c5791d8a6c2
lucas.oliveira <lucas.oliveira@grupolife.med.br>
2021-06-25 17:44:54 -0300

    Alteração de unidade de medida

--- app/Models/Unidademedida.php
+++ app/Models/Unidademedida.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Unidademedida extends Model
 {
     protected $table = 'unidademedidas';
-    protected $guarded = [];
+    public $timestamps = false;
 }
