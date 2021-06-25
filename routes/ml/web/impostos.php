342bfeb18ffed57cd5ad14b462601e87826db113
Romario Pires <romario.pires@grupolife.med.br>
2021-06-25 12:00:59 -0300

    impostos

--- routes/ml/web/impostos.php
+++ routes/ml/web/impostos.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Web\Impostos\ImpostosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('impostos', 'Api\ImpostosController@index');
-Route::post('impostos', 'Api\ImpostosController@store');
-Route::get('impostos/{imposto}', 'Api\ImpostosController@show');
-Route::put('impostos/{imposto}', 'Api\ImpostosController@update');
-Route::delete('impostos/{imposto}', 'Api\ImpostosController@destroy');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::prefix('web')->group(function () {
+        Route::get('impostos', [ImpostosController::class, 'index']);
+        Route::post('impostos', [ImpostosController::class, 'store']);
+        Route::get('impostos/{imposto}', [ImpostosController::class, 'show']);
+        Route::put('impostos/{imposto}', [ImpostosController::class, 'update']);
+        Route::delete('impostos/{imposto}', [ImpostosController::class, 'destroy']);
+    });
+});
