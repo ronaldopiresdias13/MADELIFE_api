2e7abbfea53c34cfe1f6a89c67265d88d666a9cc
Romario Pires <romario.pires@grupolife.med.br>
2021-07-13 11:21:05 -0300

    medições

--- routes/ml/web/TissRoute.php
+++ routes/ml/web/TissRoute.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('tiss/gerarXml', [TissController::class, 'gerarXml']);
+    Route::get('tiss/gerarXmlPorCliente/{cliente}', [TissController::class, 'gerarXmlPorCliente']);
 });
