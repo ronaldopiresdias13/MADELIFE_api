de4c76ac1b31f381a12be2c545b73d5a8ef998c1
Romario Pires <romario.pires@grupolife.med.br>
2021-08-02 16:04:11 -0300

    contas

--- routes/ml/web/contasbancarias.php
+++ routes/ml/web/contasbancarias.php
@@ -3,8 +3,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('contasbancarias', 'Api\ContasbancariasController@index');
-Route::post('contasbancarias', 'Api\ContasbancariasController@store');
-Route::get('contasbancarias/{contasbancaria}', 'Api\ContasbancariasController@show');
-Route::put('contasbancarias/{contasbancaria}', 'Api\ContasbancariasController@update');
-Route::delete('contasbancarias/{contasbancaria}', 'Api\ContasbancariasController@destroy');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('contasbancarias', 'Api\ContasbancariasController@index');
+    Route::post('contasbancarias', 'Api\ContasbancariasController@store');
+    Route::get('contasbancarias/{contasbancaria}', 'Api\ContasbancariasController@show');
+    Route::put('contasbancarias/{contasbancaria}', 'Api\ContasbancariasController@update');
+    Route::delete('contasbancarias/{contasbancaria}', 'Api\ContasbancariasController@destroy');
+});
