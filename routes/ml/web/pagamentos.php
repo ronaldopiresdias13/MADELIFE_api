c886f6e44d2a3fc1bc6f1a0e93323954486c753e
Romario Pires <romario.pires@grupolife.med.br>
2021-08-02 14:31:15 -0300

    Pagamentos Financeiro router

--- routes/ml/web/pagamentos.php
+++ routes/ml/web/pagamentos.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\PagamentosController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('filtroPagamentosFinanceiro', [PagamentosController::class, 'filtroPagamentoFinanceiro']);
+    Route::get('pagamentos/filtroPagamentosFinanceiro', [PagamentosController::class, 'filtroPagamentoFinanceiro']);
 });
 
 Route::get('pagamentos', 'Api\PagamentosController@index');
@@ -12,6 +12,6 @@ Route::post('pagamentos', 'Api\PagamentosController@store');
 Route::get('pagamentos/{pagamento}', 'Api\PagamentosController@show');
 Route::put('pagamentos/{pagamento}', 'Api\PagamentosController@update');
 Route::delete('pagamentos/{pagamento}', 'Api\PagamentosController@destroy');
-Route::get('pagamentosfiltro', 'Api\PagamentosController@filtro');       
+Route::get('pagamentosfiltro', 'Api\PagamentosController@filtro');
 
 // Custon
