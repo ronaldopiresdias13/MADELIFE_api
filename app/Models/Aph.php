fb3ead3b15389009ffe2a4db109efa818c2436cb
Carlos Eduardo Pasquini de Paula <carlosed2009@hotmail.com>
2021-08-03 19:08:06 -0300

    Contratos service

--- app/Models/Aph.php
+++ app/Models/Aph.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Aph extends Model
 {
     protected $guarded = [];
+
+    /**
+     * Get the user that owns the Aph
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function cidade(): BelongsTo
+    {
+        return $this->belongsTo(Cidade::class);
+    }
 }
