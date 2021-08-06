89f940f686211a3c8fa4d3dbd09732afc87675cf
lucas.oliveira <lucas.oliveira@grupolife.med.br>
2021-08-06 12:53:07 -0300

    Correção ativar e desativar profissional

--- routes/ml/web/empresaPrestador.php
+++ routes/ml/web/empresaPrestador.php
@@ -6,6 +6,9 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::prefix('web')->group(function () {
             Route::post('empresaPrestador', 'Web\EmpresaPrestador\EmpresaPrestadorController@store');
             Route::put('empresaPrestador/{empresaPrestador}', 'Web\EmpresaPrestador\EmpresaPrestadorController@update');
+
+            Route::put('empresaPrestador/ativar/{empresaPrestador}', 'Web\EmpresaPrestador\EmpresaPrestadorController@ativarPrestador');
+            Route::put('empresaPrestador/desativar/{empresaPrestador}', 'Web\EmpresaPrestador\EmpresaPrestadorController@desativarPrestador');
     });
 });
 
