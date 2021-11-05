e2037c25f3a4853e11ca0e4984436c34d1c86f9b
Carlos Eduardo Pasquini de Paula <carlosed2009@hotmail.com>
2021-11-05 14:53:42 -0300

    Alt Pacientes

--- app/Http/Controllers/Api/PacientesController.php
+++ app/Http/Controllers/Api/PacientesController.php
@@ -136,8 +136,6 @@ class PacientesController extends Controller
                     ]
                 )->id,
                 'responsavel_id' => $request['responsavel_id'],
-                'diagnostico' => $request['diagnostico'],
-                'codPaciente' => $request['codPaciente'],
                 'sexo'           => $request['sexo'],
                 'ativo'          => $request['ativo']
             ]);
@@ -259,8 +257,6 @@ class PacientesController extends Controller
                 'sexo'           => $request['sexo'],
                 'empresa_id'     => $request['empresa_id'],
                 'responsavel_id' => $request['responsavel_id'],
-                'diagnostico' => $request['diagnostico'],
-                'codPaciente' => $request['codPaciente'],
                 'ativo'          => $request['ativo'],
                 'numeroCarteira' => $request['numeroCarteira'],
             ]);
