fdaa68062370c5bd65e5c705835e3e3c2feffca7
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2021-11-10 17:00:18 -0300

    itemtabela

--- app/Http/Controllers/Web/Itemtabelaprecos/ItemtabelaprecosController.php
+++ app/Http/Controllers/Web/Itemtabelaprecos/ItemtabelaprecosController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Web\Itemtabelaprecos;
 
 use App\Http\Controllers\Controller;
 use App\Models\Itemtabelapreco;
+use App\Models\Tabelapreco;
+use App\Models\Versaotabelapreco;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ItemtabelaprecosController extends Controller
 {
+    protected $empresa_id;
+    protected $tabela;
+    protected $versao;
+    protected $file;
     /**
      * Display a listing of the resource.
      *
@@ -26,18 +33,26 @@ class ItemtabelaprecosController extends Controller
      */
     public function store(Request $request)
     {
-        foreach ($request->itenstabelapreco as $key => $item) {
-            $itemtabelapreco = new Itemtabelapreco();
-            $itemtabelapreco->versaotabelapreco_id = $item['versaotabelapreco_id'];
-            $itemtabelapreco->codigo               = $item['codigo'];
-            $itemtabelapreco->tiss                 = $item['tiss'];
-            $itemtabelapreco->tuss                 = $item['tuss'];
-            $itemtabelapreco->nome                 = $item['nome'];
-            $itemtabelapreco->preco                = $item['preco'];
-            $itemtabelapreco->save();
-        }
+        $empresa_id = $request->user()->pessoa->profissional->empresa_id;
+        $this->empresa_id = $empresa_id;
+        $this->tabela = $request->tabela['nome'];
+        $this->versao = $request->versao;
+        $this->file   = $request->file;
 
-        return $itemtabelapreco;
+        $func = $this->tabela;
+        return $this->$func();
+        // foreach ($request->itenstabelapreco as $key => $item) {
+        //     $itemtabelapreco = new Itemtabelapreco();
+        //     $itemtabelapreco->versaotabelapreco_id = $item['versaotabelapreco_id'];
+        //     $itemtabelapreco->codigo               = $item['codigo'];
+        //     $itemtabelapreco->tiss                 = $item['tiss'];
+        //     $itemtabelapreco->tuss                 = $item['tuss'];
+        //     $itemtabelapreco->nome                 = $item['nome'];
+        //     $itemtabelapreco->preco                = $item['preco'];
+        //     $itemtabelapreco->save();
+        // }
+
+        // return $itemtabelapreco;
     }
 
     /**
@@ -79,5 +94,103 @@ class ItemtabelaprecosController extends Controller
     public function destroy(Itemtabelapreco $itemtabelapreco)
     {
         $itemtabelapreco->delete;
+    }
+
+    private function brasindice()
+    {
+        $tabela = Tabelapreco::firstOrCreate([
+            'nome'       => $this->tabela,
+            'empresa_id' => $this->empresa_id
+        ]);
+
+        $versao = Versaotabelapreco::firstOrCreate([
+            'versao'         => $this->versao,
+            'tabelapreco_id' => $tabela->id
+        ]);
+
+        $md5 = md5_file($this->file);
+        $caminho = 'temp/';
+        $nome = $md5 . '.' . explode(';', explode('/', $this->file)[1])[0];
+        $file = explode(',', $this->file)[1];
+        Storage::put($caminho . $nome, base64_decode($file));
+
+        $file = fopen(storage_path("app/public/temp/") . $nome, "r");
+
+        while (!feof($file)) {
+            $linha = fgets($file);
+            $array = explode('","', $linha);
+            foreach ($array as $key => $item) {
+                $array[$key] = str_replace(['"', "\r", "\n"], "", $item);
+            }
+
+            if ($array[0]) {
+                Itemtabelapreco::updateOrCreate(
+                    [
+                        'versaotabelapreco_id' => $versao->id,
+                        'codigo'               => $array[0] . '.' . $array[2] . '.' . $array[4] // 18
+                    ],
+                    [
+                        'tiss'  => '',
+                        'tuss'  => '',
+                        'nome'  => utf8_encode($array[3]) . ' ' . $array[5], // 2
+                        'preco' => $array[9] //
+                    ]
+                );
+            }
+        }
+
+        fclose($file);
+        Storage::delete([$caminho . $nome]);
+
+        return 'Importado Brasindice!!!';
+    }
+
+    private function simpro()
+    {
+        $tabela = Tabelapreco::firstOrCreate([
+            'nome'       => $this->tabela,
+            'empresa_id' => $this->empresa_id
+        ]);
+
+        $versao = Versaotabelapreco::firstOrCreate([
+            'versao'         => $this->versao,
+            'tabelapreco_id' => $tabela->id
+        ]);
+
+        $md5 = md5_file($this->file);
+        $caminho = 'temp/';
+        $nome = $md5 . '.' . explode(';', explode('/', $this->file)[1])[0];
+        $file = explode(',', $this->file)[1];
+        Storage::put($caminho . $nome, base64_decode($file));
+
+        $file = fopen(storage_path("app/public/temp/") . $nome, "r");
+
+        while (!feof($file)) {
+            $linha = fgets($file);
+
+            $codigo = substr($linha, 267, 10);
+            $name = substr($linha, 32, 100);
+            $preco = substr($linha, 155, 8) . '.' . substr($linha, 163, 2);
+
+            if ($linha) {
+                Itemtabelapreco::updateOrCreate(
+                    [
+                        'versaotabelapreco_id' => $versao->id,
+                        'codigo'               => $codigo // $array[18]
+                    ],
+                    [
+                        'tiss'  => '',
+                        'tuss'  => '',
+                        'nome'  => utf8_encode($name), // $array[2]),
+                        'preco' => $preco // $array[6]
+                    ]
+                );
+            }
+        }
+
+        fclose($file);
+        Storage::delete([$caminho . $nome]);
+
+        return 'Importado Simpro!!!';
     }
 }
