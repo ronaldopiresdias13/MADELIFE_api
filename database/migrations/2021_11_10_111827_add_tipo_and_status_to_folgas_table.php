e000e193045690b80f28060b17463da80def3081
Romario Pires <romario.pires@grupolife.med.br>
2021-11-11 14:05:54 -0300

    Correção migrate Folga

--- database/migrations/2021_11_10_111827_add_tipo_and_status_to_folgas_table.php
+++ database/migrations/2021_11_10_111827_add_tipo_and_status_to_folgas_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Escala;
+use App\Models\Folga;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +19,15 @@ class AddTipoAndStatusToFolgasTable extends Migration
             $table->string('tipo')->nullable()->after('dataaprovacao');
             $table->string('situacao')->nullable()->after('tipo');
         });
+        $folgas = Folga::all();
+        $hoje = getdate();
+        $data = $hoje['year'] . '-' . ($hoje['mon'] < 10 ? '0' . $hoje['mon'] : $hoje['mon']) . '-' . ($hoje['mday'] < 10 ? '0' . $hoje['mday'] : $hoje['mday']);
+        foreach ($folgas as $key => $folga) {
+            $escala = Escala::find($folga->escala_id);
+            $folga->tipo     = 'Folga';
+            $folga->situacao = ($folga->substituto ? 'Substituído' : ($escala->dataentrada == $data ? 'Emergente' : 'Pendente'));
+            $folga->save();
+        }
     }
 
     /**
