fcd243efeeb5b1eb929f1811b1a950ba80f809d0
Juliana <estagiarios.madelife@hotmail.com>
2021-11-16 11:36:52 -0300

    correção observação em editar escalas part2

--- app/Http/Controllers/Api/EscalasController.php
+++ app/Http/Controllers/Api/EscalasController.php
@@ -267,7 +267,6 @@ class EscalasController extends Controller
         $escala->motivoadicional       = $request->motivoadicional;
         $escala->valordesconto         = $request->valordesconto;
         $escala->motivodesconto        = $request->motivodesconto;
-        $escala->observacao           = $request->observacao;
         $escala->save();
     }
 
