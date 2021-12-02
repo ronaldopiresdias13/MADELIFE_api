6fe34cde81e5df0139e018bd147f5331911fb6c3
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2021-12-01 21:20:34 -0300

    os

--- app/Http/Controllers/Api/OrdemservicosController.php
+++ app/Http/Controllers/Api/OrdemservicosController.php
@@ -349,7 +349,7 @@ class OrdemservicosController extends Controller
         $result = Ordemservico::with([
             'orcamento.cidade', 'orcamento.evento', 'orcamento.cliente.pessoa', 'profissional.pessoa', 'orcamento' => function ($query) {
                 $query->with(['servicos.servico', 'produtos.produto', 'homecare' => function ($query) {
-                    $query->with(['paciente.pessoa', 'paciente.responsavel.pessoa']);
+                    $query->with(['paciente.pessoa', 'paciente.pessoa.enderecos', 'paciente.responsavel.pessoa']);
                 }]);
             }
         ])
