4ff7be070a98ed0eadf72a35c107ee3d2379aa62
Carlos Eduardo Pasquini de Paula <carlosed2009@hotmail.com>
2021-12-27 11:15:17 -0300

    Adicionado novos dados no cadastro de empresas

--- app/Http/Controllers/Web/Empresas/EmpresasController.php
+++ app/Http/Controllers/Web/Empresas/EmpresasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web\Empresas;
 
 use App\Http\Controllers\Controller;
 use App\Models\Empresa;
+use App\Models\EmpresaDados;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -28,7 +29,7 @@ class EmpresasController extends Controller
     public function store(Request $request)
     {
         DB::transaction(function () use ($request) {
-            Empresa::create([
+            $empresa = Empresa::create([
                 'razao'                    => $request['razao'],
                 'cnpj'                     => $request['cnpj'],
                 'ie'                       => $request['ie'],
@@ -48,6 +49,21 @@ class EmpresasController extends Controller
                 'site'                     => $request['site'],
                 'email'                    => $request['email'],
             ]);
+            EmpresaDados::firstOrCreate(
+                [
+                    'codigo'              =>  $request['empresa_dado']['codigo'],
+                    'agencia'             =>  $request['empresa_dado']['agencia'],
+                    'digito_agencia'      =>  $request['empresa_dado']['digito_agencia'],
+                    'conta'               => $request['empresa_dado']['conta'],
+                    'digito_conta'        =>  $request['empresa_dado']['digito_conta'],
+                    'convenio'            => $request['empresa_dado']['convenio'],
+                    'convenio_externo'    => $request['empresa_dado']['convenio_externo'],
+                    'nome'                =>  $request['empresa_dado']['nome'],
+                    'nome_empresa'        => $empresa->razao,
+                    'cnpj'                => $empresa->cnpj,
+                    'empresa_id'          => $empresa->id,
+                ]
+            );
         });
     }
 
@@ -61,7 +77,7 @@ class EmpresasController extends Controller
     {
         // $user = $request->user();
         // $empresa_id = $user->pessoa->profissional->empresa_id;
-
+        $empresa->empresa_dado;
         // $result = Empresa::find($empresa_id);
         return $empresa;
     }
@@ -96,6 +112,24 @@ class EmpresasController extends Controller
                 'site'                     => $request['site'],
                 'email'                    => $request['email'],
             ]);
+            EmpresaDados::updateOrCreate(
+                [
+                    'id'                 => $request['empresa_dado']['id']
+                ],
+                [
+                    'codigo'              =>  $request['empresa_dado']['codigo'],
+                    'agencia'             =>  $request['empresa_dado']['agencia'],
+                    'digito_agencia'      =>  $request['empresa_dado']['digito_agencia'],
+                    'conta'               => $request['empresa_dado']['conta'],
+                    'digito_conta'        =>  $request['empresa_dado']['digito_conta'],
+                    'convenio'            => $request['empresa_dado']['convenio'],
+                    'convenio_externo'    => $request['empresa_dado']['convenio_externo'],
+                    'nome'                =>  $request['empresa_dado']['nome'],
+                    'nome_empresa'        => $empresa->razao,
+                    'cnpj'                => $empresa->cnpj,
+                    'empresa_id'          => $empresa->id,
+                ]
+            );
         });
     }
 
