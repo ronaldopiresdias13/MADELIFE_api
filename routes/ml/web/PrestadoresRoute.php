96ec1d497a25a3087ee2cf02faa568149ae1421b
Juliana <julianabarbeiro.jb@gmail.com>
2022-02-02 15:33:48 -0300

    adição listNomePrestadoresFinanceiro

--- routes/ml/web/PrestadoresRoute.php
+++ routes/ml/web/PrestadoresRoute.php
@@ -10,7 +10,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('web/prestadores/empresaPrestador/listaPrestadoresPorEmpresaIdEStatus', [EmpresaPrestadorController::class, 'listaPrestadoresPorEmpresaIdEStatus']);
     Route::get('web/prestadores/buscaPrestadorComServicosPrestadosNaEmpresa/{prestador}', [PrestadoresController::class, 'buscaPrestadorComServicosPrestadosNaEmpresa']);
     Route::get('web/prestadores/buscaprestadorexterno/{prestador}', [PrestadoresController::class, 'buscaprestadorexterno']);
-    Route::get('web/prestadores/listNomePrestadores', 'Api\Web\PrestadoresController@listNomePrestadores');
+    Route::get('web/prestadores/listNomePrestadoresFinanceiro', 'Api\Web\PrestadoresController@listNomePrestadoresFinanceiro');
     Route::get('web/prestadores/listPrestadoresComFormacoes', 'Api\Web\PrestadoresController@listPrestadoresComFormacoes');
     Route::get('web/prestadores/buscaPrestadoresPorCliente', [PrestadoresController::class, 'buscaPrestadoresPorCliente']);
     Route::get('prestadores/atribuicao', 'Api\Web\PrestadoresController@buscaprestadoresporfiltro');
