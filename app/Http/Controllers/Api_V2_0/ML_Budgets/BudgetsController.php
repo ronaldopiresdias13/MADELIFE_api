a4ebc1a33a1e2db7c7f667eeb9904965d45046b3
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2022-02-10 15:20:13 -0300

    budgets

--- app/Http/Controllers/Api_V2_0/ML_Budgets/BudgetsController.php
+++ app/Http/Controllers/Api_V2_0/ML_Budgets/BudgetsController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Api_V2_0\Budget;
 use Illuminate\Http\Request;
 
-class Budgets extends Controller
+class BudgetsController extends Controller
 {
     /**
      * Display a listing of the resource.
---
+++ routes/ml/web/budgets.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Api_V2_0\ML_Budgets\Budgets;
+use App\Http\Controllers\Api_V2_0\ML_Budgets\BudgetsController;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::prefix('web')->group(function () {
+       Route::get('budgets', [BudgetsController::class, 'index']);
+       Route::post('budget', [BudgetsController::class, 'store']);
+       Route::put('budget/{budget}', [BudgetsController::class, 'update']);
+       Route::get('budget/{budget}', [BudgetsController::class, 'show']);
+    //    Route::delete('budgets/excluirItemPacoteServico/{budgetservice}', [BudgetsController::class, 'excluirItemPacoteServico']);
+    //    Route::delete('budgets/excluirItemPacoteProduto/{packageProduct}', [BudgetsController::class, 'excluirItemPacoteProduto']);
+    });
+});
