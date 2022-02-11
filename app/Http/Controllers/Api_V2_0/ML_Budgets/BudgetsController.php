5be1e8280f2ab65588cfc6899c8ad9dd0e869e26
Ronaldo Pires <ronaldo.pires@grupolife.med.br>
2022-02-11 09:38:44 -0300

    budgets-routes

--- app/Http/Controllers/Api_V2_0/ML_Budgets/BudgetsController.php
+++ app/Http/Controllers/Api_V2_0/ML_Budgets/BudgetsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api_V2_0\ML_Budgets;
 use App\Http\Controllers\Controller;
 use App\Models\Api_V2_0\Budget;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BudgetsController extends Controller
 {
@@ -31,7 +32,35 @@ class BudgetsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $empresa_id = $request->user()->pessoa->profissional->empresa_id;
+
+        DB::transaction(function () use ($request, $empresa_id){
+            $budget = Budget::create([
+                'company_id' => $empresa_id,
+                'packages_id' => $request->packages_id,
+                'city_id' => $request->city_id,
+                'addition_code' => $request->addition_code,
+                'description' => $request->description,
+                'objective' => $request->objective,
+                'accepted' => $request->accepted,
+                'status' => $request->status,
+                'budget_number' => $request->budget_number,
+                'budget_type' => $request->budget_type,
+                'process_number' => $request->process_number,
+                'date' => $request->date,
+                'situation' => $request->situation,
+                'quantity' =>  $request->quantity,
+                'version' => $request->version,
+                'unity' => $request-> unity
+            ]);
+        });
+        return response()->json([
+            'alert' => [
+                'title' => 'Ok!',
+                'text' => 'Orçamento cadastrado com sucesso!'
+            ]
+        ], 200)
+            ->header('Content-Type', 'application/json');
     }
 
     /**
@@ -40,9 +69,11 @@ class BudgetsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Budget $budget)
     {
-        //
+        $budget->package;
+        $budget->cidade;
+        return $budget;
     }
 
     /**
@@ -52,9 +83,37 @@ class BudgetsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Budget $budget)
     {
-        //
+        $empresa_id = $request->user()->pessoa->profissional->empresa_id;
+
+        DB::transaction(function () use ($request, $budget, $empresa_id){
+            $budget->update([
+                'company_id' => $empresa_id,
+                'packages_id' => $request->packages_id,
+                'city_id' => $request->city_id,
+                'addition_code' => $request->addition_code,
+                'description' => $request->description,
+                'objective' => $request->objective,
+                'accepted' => $request->accepted,
+                'status' => $request->status,
+                'budget_number' => $request->budget_number,
+                'budget_type' => $request->budget_type,
+                'process_number' => $request->process_number,
+                'date' => $request->date,
+                'situation' => $request->situation,
+                'quantity' =>  $request->quantity,
+                'version' => $request->version,
+                'unity' => $request-> unity
+            ]);
+        });
+        return response()->json([
+            'alert' => [
+                'title' => 'Ok!',
+                'text' => 'Orçamento cadastrado com sucesso!'
+            ]
+        ], 200)
+            ->header('Content-Type', 'application/json');
     }
 
     /**
